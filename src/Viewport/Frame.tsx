3e38c06588041f5aeda69d12692be95f170a7a3f
Alan Luo <me@alanluo.com>
2022-12-01 12:22:30 -0500

    added scrollmax

--- src/Viewport/Frame.tsx
+++ src/Viewport/Frame.tsx
@@ -5,20 +5,20 @@ import { CharPixel } from "../CharPixelLib/CharPixel";
 import { Position } from "../Utils/Position";
 
 function Framer({ x, y }: Position) {
-  return <CharPixel char="@" z={0} x={x} y={y} />;
+  return <CharPixel char="@" z={0} x={x} y={y} color="red" />;
 }
 
 export function Frame() {
   const gM = useGameManager();
   const [width] = useState<number>(gM.viewportManager.getWidth());
-  const [height] = useState<number>(gM.viewportManager.getWidth());
+  const [height] = useState<number>(gM.viewportManager.getHeight());
 
   return (
     <>
       <Framer x={0} y={0} />
-      <Framer x={0} y={height} />
-      <Framer x={width} y={0} />
-      <Framer x={width} y={height} />
+      <Framer x={0} y={height - 1} />
+      <Framer x={width - 1} y={0} />
+      <Framer x={width - 1} y={height - 1} />
     </>
   );
 }
--- src/Viewport/ViewportManager.ts
+++ src/Viewport/ViewportManager.ts
@@ -1,8 +1,10 @@
 import autoBind from "auto-bind";
 import { CANVAS_WIDTH, CANVAS_HEIGHT, PIXEL_WIDTH } from "../Utils/consts";
 import { Position } from "../Utils/Position";
+import { clamp } from "../Utils/utils";
 
 export const unit_wToS = (w: number) => w * PIXEL_WIDTH;
+export const unit_sToW = (w: number) => w / PIXEL_WIDTH;
 
 export class ViewportManager {
   private width: number = CANVAS_WIDTH;
@@ -19,6 +21,7 @@ export class ViewportManager {
     this.container = container;
   }
 
+
   // basic getters
   public getWidth(): number { return this.width; }
   public getHeight(): number { return this.height; }
@@ -34,6 +37,15 @@ export class ViewportManager {
     return this.container ? this.container.scrollTop : 0;
   }
 
+  private scrollMax(): Position {
+    const maxScrollX_w = Math.ceil(this.width - unit_sToW(window.innerWidth));
+    const maxScrollY_w = Math.ceil(this.height - unit_sToW(window.innerHeight));
+
+
+    return { x: unit_wToS(maxScrollX_w), y: unit_wToS(maxScrollY_w) }
+    // return { x: Infinity, y: Infinity };
+  }
+
   // math
   public worldToScreen(pos: Position): Position {
     return {
@@ -44,7 +56,9 @@ export class ViewportManager {
 
   // scroll fns
   public scroll(x: number, y: number) {
-    if (this.container) this.container.scroll(x, y);
+    if (!this.container) return;
+    const { x: maxX, y: maxY } = this.scrollMax();
+    this.container.scroll(clamp(x, 0, maxX), clamp(y, 0, maxY));
   }
 
   public scrollToCenter() {
