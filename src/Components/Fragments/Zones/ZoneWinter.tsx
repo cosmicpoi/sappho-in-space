dd7fff2bd75a61b24441f9a1e886f1c2dfa55c1f
Alan Luo <me@alanluo.com>
2022-12-23 14:53:14 -0500

    added maze infra

--- src/Components/Fragments/Zones/ZoneWinter.tsx
+++ src/Components/Fragments/Zones/ZoneWinter.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { useCenter } from "../../../Utils/Hooks";
-import { Fragment20 } from "../FragmentComponents/Fragment20to29";
+import { Fragment20Puzzle } from "../../Puzzles/Fragmet20Puzzle";
 
 export function ZoneWinter() {
   const { x: cx, y: cy } = useCenter();
 
   return (
     <>
-      <Fragment20 x={cx - 430} y={cy - 440} />
+      <Fragment20Puzzle x={cx - 430} y={cy - 440} z={0} />
     </>
   );
 }
---
+++ src/Components/Puzzles/Fragmet20Puzzle.tsx
@@ -0,0 +1,124 @@
+import * as React from "react";
+import { useMemo } from "react";
+import { CharPixel } from "../../CharPixelLib/CharPixel";
+import { FragmentKey } from "../../Data/FragmentData";
+import { useLines } from "../../Utils/Hooks";
+import { Position3DR } from "../../Utils/types";
+import { toN } from "../../Utils/utils";
+import { fragment20text } from "../Fragments/FragmentText/FragmentText20to29";
+import { FragmentLabel } from "../Labels";
+import { LineText } from "../LineText";
+import { Wall } from "../Wall";
+
+function End20Corridor({ x, y, z }: Position3DR) {
+  const lines = useLines(fragment20text);
+
+  const w1 = useMemo(() => ({ x: x, y: y, width: 1, height: 48 }), [x, y]);
+  const w2 = useMemo(() => ({ x: x + 28, y: y, width: 1, height: 48 }), [x, y]);
+
+  return (
+    <>
+      <Wall hitbox={w1} />
+      <Wall hitbox={w2} />
+
+      {lines.map((line: string, i: number) => (
+        <>
+          <CharPixel x={x} y={y + i * 2} z={z} char={"]"} />
+          <LineText
+            x={x + 1}
+            y={y + i * 2}
+            z={z}
+            text={line.substring(1, line.length)}
+            opacity={0.5}
+          />
+          <CharPixel x={x + 28} y={y + i * 2} z={z} char={"["} />
+        </>
+      ))}
+      <LineText x={x + 4} y={y + 10} z={z + 1} text={"black earth"} />
+    </>
+  );
+}
+
+type Pt = [number, number]; // [0, dim] (inclusive)
+type Edge = [Pt, Pt];
+
+const dim = 10;
+const cellSize = 14;
+const corners: Pt[] = (() => {
+  const c: Pt[] = [];
+  for (let i = 0; i <= dim; i++)
+    for (let j = 0; j <= dim; j++) {
+      if (
+        !(j === 0 && i === 1) &&
+        !(j === 1 && i === 1) &&
+        !(j === dim && i === dim - 1) &&
+        !(j === dim - 1 && i === dim - 1)
+      )
+        c.push([i, j]);
+    }
+  return c;
+})();
+
+const edges: Edge[] = (() => {
+  const e: Edge[] = [];
+  for (let i = 0; i < dim; i++) {
+    // prettier-ignore
+    if (i > 1) e.push([[i, 0], [i + 1, 0]]);
+    // prettier-ignore
+    e.push([[0, i], [0, i + 1]]);
+    // prettier-ignore
+    e.push([[dim, i], [dim, i + 1]]);
+    // prettier-ignore
+    if (i < dim - 2) e.push([[i, dim], [i + 1, dim]]);
+  }
+  return e;
+})();
+// prettier-ignore
+function Maze({ x, y, z }: Position3DR) {
+  return (
+    <>
+      {corners.map(([i, j]) => (
+        <CharPixel
+          key={`${i}-${j}`}
+          x={x + i * cellSize}
+          y={y + j * cellSize}
+          z={z}
+          char="+"
+        />
+      ))}
+      {edges.map(([[i1, j1], [i2, j2]]) => (
+        <React.Fragment key={`${i1}-${j1}.${i2}-${j2}`}>
+          <Wall
+            hitbox={{
+              x: x + i1 * cellSize,
+              y: y + j1 * cellSize,
+              width: i2 - i1 === 0 ? 1 : cellSize + 1,
+              height: j2 - j1 === 0 ? 1 : cellSize + 1,
+            }}
+          />
+          {toN(cellSize)
+            .filter((k) => k > 0)
+            .map((k) => (
+              <CharPixel
+                key={k}
+                x={x + i1 * cellSize + k * (i2 - i1)}
+                y={y + j1 * cellSize + k * (j2 - j1)}
+                z={z}
+                char={k % 2 === 0 ? (i2 - i1 === 0 ? "|" : "-") : undefined}
+              />
+            ))}
+        </React.Fragment>
+      ))}
+    </>
+  );
+}
+
+export function Fragment20Puzzle({ x, y, z }: Position3DR) {
+  return (
+    <>
+      <FragmentLabel x={x} y={y - 4} z={z} fkey={FragmentKey.F20} decor />
+      <End20Corridor x={x} y={y} z={z} />
+      <Maze x={x} y={y + 48} z={z} />
+    </>
+  );
+}
