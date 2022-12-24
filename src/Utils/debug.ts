ffb611cbd092fe0b3ffe4db0ba2c7653ec1c98a8
Alan Luo <me@alanluo.com>
2022-12-24 03:54:15 -0500

    Update debug.ts

--- src/Utils/debug.ts
+++ src/Utils/debug.ts
@@ -3,6 +3,7 @@ import { Position } from "./types";
 export const DEBUG_WALL = false;
 export const DEBUG_SCROLL = false;
 export const DEBUG_ENVIRONMENT = false;
-export const DEBUG_NOSAVE = true;
+export const DEBUG_NOSAVE = false;
+export const DEBUG_SOLVE = false;
 
 export const DEBUG_START_POS: Position | undefined = undefined;
